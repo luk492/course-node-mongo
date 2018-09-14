cd45836d928be878d03fd8ff2fb6971e92e6420a
Luka Kajtes <lukakajtes@Lukas-MacBook-Pro.local>
2018-09-14 20:22:09 +0200

    Add POST /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGODB_URI, { 
+    useNewUrlParser: true
+});
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -86,6 +86,19 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+app.post('/users', (req, res) => {
+    var user = new User(
+        _.pick(req.body,['email','password'])
+    );
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 module.exports = {
     app
 };
